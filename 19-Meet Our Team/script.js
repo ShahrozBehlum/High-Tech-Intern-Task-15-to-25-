d567ce3fc15329164a0f195d8a90860b51ec9c7c
Shahroz <Shahrozbehlum@gmail.com>
2025-08-09 11:38:59 +0500

    Add files via upload

---
+++ 19-Meet Our Team/script.js
@@ -0,0 +1,14 @@
+// Scroll to Top Button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
